0ae0cd0b8b400002c6366413a2bc1619fb3b4a9e
joe <brakejoe9@gmail.com>
2024-04-13 04:28:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+  
+  const winningYearObj = record.find(obj => obj.result === "W");
+  
+  if (winningYearObj) {
+      return winningYearObj.year;
+  } else {
+      
+      return undefined;
+  }
+}
